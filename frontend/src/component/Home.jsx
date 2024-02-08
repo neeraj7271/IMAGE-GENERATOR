edeeb441aaf8006f8d7fe316a11646e71483cf1b
Bhaaaarat <bharatmangal10@gmail.com>
2024-02-08 18:04:35 +0530

    comment added

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Form from "./Form";
 
 function Home() {
-
+/*{'hkhkhk'}*/
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-blue-400">
         <Form />
